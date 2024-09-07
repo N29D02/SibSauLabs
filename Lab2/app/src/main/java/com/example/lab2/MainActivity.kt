e756bcbc2437f082c75b9715aacac592c2c9531f
617HoZ <61546803+N29D02@users.noreply.github.com>
2024-09-07 19:36:08 +0700

    Getting clone of previous lab

--- Lab2/app/src/main/java/com/example/lab2/MainActivity.kt
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.lab2
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab2.ui.theme.Lab2Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +32,46 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Lab2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Lab1WidgetPreview()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Lab1WidgetPreview() {
     Lab2Theme {
-        Greeting("Android")
+        val context = LocalContext.current
+        var inputString by remember { mutableStateOf("") }
+
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(modifier = Modifier.padding(innerPadding).fillMaxSize(), verticalArrangement = Arrangement.Center) {
+                Text(
+                    text = "Lab1",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxWidth()
+                        .padding(top = 12.dp)
+                )
+                TextField(modifier = Modifier
+                    .padding(top = 12.dp, start = 36.dp, end = 36.dp)
+                    .fillMaxWidth(),
+                    value = inputString,
+                    onValueChange = {newText -> inputString = newText})
+
+                Button(modifier = Modifier.align(Alignment.CenterHorizontally).padding(12.dp),
+                    onClick = {
+                        if (inputString.lowercase().contains('d')) {
+                            Toast.makeText(context, "Это Дуб", Toast.LENGTH_SHORT).show()
+                        }
+                        else{
+                            Toast.makeText(context, "Это что то явно другое", Toast.LENGTH_SHORT).show()
+                        }
+                    }) {
+                    Text("Проверить")
+                }
+            }
+        }
     }
 }
