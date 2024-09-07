8f2dfada9db41c92a1cbffd9bbbc8a8a68b76f41
617HoZ <61546803+N29D02@users.noreply.github.com>
2024-09-07 18:57:19 +0700

    Adding textField and button for checking input string

--- Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.lab1
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,18 +9,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lab1.ui.theme.Lab1Theme
+import java.time.Duration
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,8 +62,9 @@ fun Lab1Widget(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun Lab1WidgetPreview() {
     Lab1Theme {
+        val context = LocalContext.current
         var inputString by remember {mutableStateOf("")}
-        
+
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
             Column(modifier = Modifier.padding(innerPadding)) {
                 Lab1Widget(
@@ -67,6 +74,23 @@ fun Lab1WidgetPreview() {
                         .fillMaxWidth()
                         .padding(top = 12.dp)
                 )
+                TextField(modifier = Modifier
+                    .padding(top = 12.dp)
+                    .fillMaxWidth(),
+                value = inputString,
+                onValueChange = {newText -> inputString = newText})
+
+                Button(modifier = Modifier.align(Alignment.CenterHorizontally).padding(12.dp),
+                onClick = {
+                    if (inputString.contains('d')) {
+                        Toast.makeText(context, "Это Дуб", Toast.LENGTH_SHORT).show()
+                    }
+                    else{
+
+                    }
+                    }) {
+                    Text("Проверить")
+                }
             }
         }
     }
