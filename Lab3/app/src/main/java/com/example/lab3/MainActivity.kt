05957ab5ef14f81c7a383d76396bea65ee01a97f
617HoZ <61546803+N29D02@users.noreply.github.com>
2024-09-13 03:39:40 +0700

    ...

--- Lab3/app/src/main/java/com/example/lab3/MainActivity.kt
+++ Lab3/app/src/main/java/com/example/lab3/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab3.ui.theme.Lab3Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +28,35 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Lab3Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Lab3WidgetPreview()
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+fun Lab3WidgetPreview(){
     Lab3Theme {
-        Greeting("Android")
+        var inputString by remember { mutableStateOf("")}
+        var outputString by remember { mutableStateOf("")}
+
+        Scaffold(Modifier.fillMaxSize()){ innerPadding ->
+            Column(
+                Modifier
+                    .padding(innerPadding).padding(horizontal = 88.dp)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center){
+                Text(
+                    text = outputString,
+                    textAlign =  TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                TextField(value = inputString, modifier = Modifier.fillMaxWidth(), onValueChange = {
+                    newText -> inputString = newText
+
+                    
+                })
+            }
+        }
     }
 }
