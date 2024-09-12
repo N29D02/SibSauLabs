a77236b90e8f28ede8e64c747c1c3de4dadbc627
617HoZ <61546803+N29D02@users.noreply.github.com>
2024-09-13 04:20:14 +0700

    ///

--- Lab4/app/src/main/java/com/example/lab4/MainActivity.kt
+++ Lab4/app/src/main/java/com/example/lab4/MainActivity.kt
@@ -9,16 +9,22 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,17 +47,24 @@ class MainActivity : ComponentActivity() {
 fun Lab4WidgetPreview(){
     Lab4Theme {
         Scaffold(Modifier.fillMaxSize(), topBar = {
-            TopAppBar(title = { Text("GeoQuiz") })
+            TopAppBar(colors = topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary,
+            ),
+                title = { Text("GeoQuiz") })
 
         }){ innerPadding ->
 
             Column(
                 Modifier
                     .padding(innerPadding)
-                    .padding(horizontal = 88.dp)
+                    .padding(horizontal = 12.dp)
                     .fillMaxSize(),
-                verticalArrangement = Arrangement.Center){
-
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally){
+                Button(modifier = Modifier, onClick = { /*TODO*/ }) {
+                    Text("Start")
+                }
             }
         }
     }
