73983ed8b0061f1d2877017ae75b5f51f1262c04
617HoZ <61546803+N29D02@users.noreply.github.com>
2024-09-13 02:52:37 +0700

    some commit

--- Lab22/app/src/main/java/com/example/lab2/MainActivity.kt
+++ Lab22/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab2.ui.theme.Lab2Theme
 
@@ -28,10 +30,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Lab2WidgetPreview(){
     Lab2Theme {
+        var sum by remember { mutableStateOf(0)}
+
+        val o_accuracy = 0.00001f
+
+
         Scaffold(Modifier.fillMaxSize()){ innerPadding ->
             Column(Modifier.padding(innerPadding).fillMaxSize(),
                 verticalArrangement = Arrangement.Center){
-
+                Text(
+                    text = "",
+                    textAlign =  TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
             }
         }
     }
