c6e8483fb628017778ccb64dcb0a23a88cec13a5
617HoZ <61546803+N29D02@users.noreply.github.com>
2024-09-21 07:38:28 +0700

    final commit for lab4_2

--- Lab4_2/app/src/main/java/com/example/lab4_2/CheatActivity.kt
+++ Lab4_2/app/src/main/java/com/example/lab4_2/CheatActivity.kt
@@ -8,8 +8,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -23,10 +21,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lab4_2.ui.theme.Lab4_2Theme
-import com.example.lab4_2.ui.theme.QuizQuestions
 
 class CheatActivity : ComponentActivity() {
     private val cheatViewModel: CheatViewModel by viewModels()
