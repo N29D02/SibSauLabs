98f2bce86a1b9385adf78457efc35dff4b36ddb5
617HoZ <61546803+N29D02@users.noreply.github.com>
2024-09-23 01:42:39 +0700

    added main part of MainActivity

--- Lab5/app/src/main/java/com/example/lab5/MainActivity.kt
+++ Lab5/app/src/main/java/com/example/lab5/MainActivity.kt
@@ -4,46 +4,73 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab5.ui.theme.Lab5Theme
 
 class MainActivity : ComponentActivity() {
+    private val mainViewModel: MainActivityVM by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-
         setContent {
-            Lab5Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MainWidget()
+
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainWidget(){
+    //val mainViewModel: MainActivityVM by ComponentActivity().viewModels()
+    var mainViewModel_hourValue = ""
+    var mainViewModel_discountValue = 0.1f
+    Lab5Theme {
+        TopAppBar(title = { Text("DiscountApp") })
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(
+                Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
+                TextField(value = mainViewModel_hourValue, onValueChange = {
+
+                })
+                Text("Количество часов")
+                Slider(value = mainViewModel_discountValue, modifier = Modifier.padding(horizontal = 32.dp), onValueChange = {
+                    
+                })
+                Text("Скидка: $mainViewModel_discountValue")
+                Button(onClick = {}){
+                    Text("Расчитать")
                 }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Lab5Theme {
-        Greeting("Android")
-    }
+@Preview
+
+fun MainWidgetPreview(){
+    //val mainViewModel: MainActivityVM by ComponentActivity().viewModels()
+    MainWidget()
 }
---
+++ Lab5/app/src/main/java/com/example/lab5/MainActivityVM.kt
@@ -0,0 +1,13 @@
+package com.example.lab5
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class MainActivityVM: ViewModel() {
+    var hoursRawValue by mutableStateOf("")
+    var discountValue by mutableFloatStateOf(0.1f)
+}
