a813ad98109bf64a36dec4381f400319797f109b
617HoZ <61546803+N29D02@users.noreply.github.com>
2024-09-28 05:45:05 +0700

    2nd version of crime report menu

--- Lab6/app/src/main/java/com/example/lab6/CriminalReportMenu.kt
+++ Lab6/app/src/main/java/com/example/lab6/CriminalReportMenu.kt
@@ -1,17 +1,38 @@
 package com.example.lab6
 
+import android.graphics.Paint
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DatePicker
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,10 +40,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -39,20 +65,49 @@ fun CriminalReportWidget(){
 
     Column(
         Modifier
-            .padding(top = 16.dp)
-            .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(16.dp) )
+            .padding(16.dp)
+            .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(8.dp) )
     {
-        Text("text")
-        Button(onClick = { selectImageLauncher.launch("image/*") }) {
+        Box(
+            modifier = Modifier
+                .size(150.dp)
+                .clip(RoundedCornerShape(360.dp))
+                .background(Color.Gray)
+        ) {
+            if (selectedImageUri != null) {
+                Image(
+                    painter = rememberAsyncImagePainter(model = selectedImageUri),
+                    contentDescription = "Selected Image",
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+            }
 
+            IconButton(modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.Center)
+                .background(Color.Black.copy(alpha = 0.2f)), onClick = {
+                selectImageLauncher.launch("image/*")
+            }){
+                Icon(modifier = Modifier.size(36.dp), imageVector = Icons.Outlined.Add, contentDescription = "IconPhotoAdd", tint = Color.White)
+            }
         }
-
-        selectedImageUri?.let { uri ->
-            Image(
-                painter = rememberAsyncImagePainter(model = uri),
-                contentDescription = "Selected Image",
-                modifier = Modifier.size(200.dp)
-            )
+        TextField(modifier = Modifier.fillMaxWidth(), value = "", onValueChange = {}, placeholder = {Text("Title")})
+        Text("Details")
+        Spacer(modifier = Modifier
+            .height(1.dp)
+            .fillMaxWidth()
+            .background(Color.Black))
+        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Start){
+            Checkbox(checked = true, onCheckedChange = {})
+            Text("Solved")
+        }
+        Button(modifier = Modifier, onClick = {  }, shape = RoundedCornerShape(8.dp)) {
+            Text("Choose suspect")
+        }
+        Button(modifier = Modifier, onClick = {  }, shape = RoundedCornerShape(8.dp)) {
+            Text("Send crime report")
         }
     }
 }
