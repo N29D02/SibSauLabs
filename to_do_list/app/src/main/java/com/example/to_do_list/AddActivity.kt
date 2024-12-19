af83c183a14ebcc24687b0e691792763ee97cbaa
617HoZ <61546803+N29D02@users.noreply.github.com>
2024-12-20 03:19:34 +0700

    Add AddActivity.kt

---
+++ to_do_list/app/src/main/java/com/example/to_do_list/AddActivity.kt
@@ -0,0 +1,58 @@
+package com.example.to_do_list
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.to_do_list.ui.theme.To_do_listTheme
+import kotlinx.coroutines.flow.first
+
+class AddActivity : ComponentActivity() {
+
+    private lateinit var database: AppDatabase
+    private lateinit var taskDao: TaskDao
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        database = AppDatabase.getDatabase(applicationContext)
+        taskDao = database.taskDao()
+
+        setContent {
+            To_do_listTheme {
+                
+            }
+        }
+    }
+}
