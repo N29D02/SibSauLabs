19498523c1e73a318c49a106ef5495a9fae3ab25
617HoZ <61546803+N29D02@users.noreply.github.com>
2024-12-20 03:31:49 +0700

    Append androidManifest and update MainActivity

--- to_do_list/app/src/main/java/com/example/to_do_list/MainActivity.kt
+++ to_do_list/app/src/main/java/com/example/to_do_list/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
@@ -34,7 +35,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -67,7 +71,8 @@ fun TaskListScreen(taskDao: TaskDao) {
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
-
+                    val intent = Intent(context, AddActivity::class.java)
+                    context.startActivity(intent)
                 }
             ) {
                 Icon(Icons.Default.Add, contentDescription = "Add Task")
@@ -80,7 +85,14 @@ fun TaskListScreen(taskDao: TaskDao) {
                 .padding(paddingValues)
                 .fillMaxSize()
         ) {
-
+            items(tasks) { task ->
+                TaskItem(task = task, onDelete = {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        taskDao.deleteTask(it)
+                        tasks.remove(it)
+                    }
+                })
+            }
         }
     }
 }
